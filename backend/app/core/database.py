678fbeb1a1c1ac5685ac4784c5624e72a7d939e6
edugbau <edugb@uma.es>
2025-12-12 12:41:15 +0100

    tomar por

--- backend/app/core/database.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from odmantic import AIOEngine
-from app.core.config import settings
-
-client = AsyncIOMotorClient(settings.MONGO_URI)
-engine = AIOEngine(client=client, database=settings.DATABASE_NAME)
